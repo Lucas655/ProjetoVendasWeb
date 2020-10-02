b1b1a841e88b7a9555a1eec055cde429eb24ae21
Lucas655 <desouza655@gmail.com>
2020-10-02 13:22:33 -0300

    Add others entities + Migration

--- VendasWebMvc/Data/VendasWebMvcContext.cs
+++ VendasWebMvc/Data/VendasWebMvcContext.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using VendasWebMvc.Models;
+using VendasWebMvc.Models.Enums;
 
 namespace VendasWebMvc.Data
 {
@@ -13,7 +14,9 @@ namespace VendasWebMvc.Data
             : base(options)
         {
         }
-
-        public DbSet<VendasWebMvc.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        
     }
 }
