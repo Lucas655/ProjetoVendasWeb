60ebe988398c3d28d4c6097bc4ca0d345aa5b7d4
Lucas655 <desouza655@gmail.com>
2020-10-06 13:31:32 -0300

    Foreign Key not NULL - Integr. Referencial

--- VendasWebMvc/Models/Services/SellerService.cs
+++ VendasWebMvc/Models/Services/SellerService.cs
@@ -23,6 +23,7 @@ namespace VendasWebMvc.Models.Services
 
         public void Insert(Seller obj)
         {
+            obj.Department = _context.Department.First();
             _context.Add(obj);
             _context.SaveChanges();
         }
