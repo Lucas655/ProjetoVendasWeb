4c73148c938a7b98545c0bda6754c44723f872e2
Lucas655 <desouza655@gmail.com>
2020-10-06 13:22:28 -0300

    Insercao de formulario para cadastros de vendedores

--- VendasWebMvc/Controllers/SellersController.cs
+++ VendasWebMvc/Controllers/SellersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using VendasWebMvc.Models;
 using VendasWebMvc.Models.Services;
 
 namespace VendasWebMvc.Controllers
@@ -21,6 +22,19 @@ namespace VendasWebMvc.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
 
 
