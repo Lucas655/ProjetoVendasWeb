46795bd53efb6aeb7de79279c8ab310ffeff5026
Lucas655 <desouza655@gmail.com>
2020-10-07 12:54:38 -0300

    Adicionando detalhes e eager loading

--- VendasWebMvc/Models/Services/SellerService.cs
+++ VendasWebMvc/Models/Services/SellerService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -23,7 +24,7 @@ namespace VendasWebMvc.Models.Services
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
 
         public void Remove(int id)
