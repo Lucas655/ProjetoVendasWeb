37e985e284a0738e3a38476c52acc41b58a7bf4f
Lucas655 <desouza655@gmail.com>
2020-10-08 11:52:28 -0300

    Validacoes de campos

--- VendasWebMvc/Models/Seller.cs
+++ VendasWebMvc/Models/Seller.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 namespace VendasWebMvc.Models
@@ -9,11 +10,30 @@ namespace VendasWebMvc.Models
     {
 
         public int Id { get; set; }
+
+        [Required(ErrorMessage ="{0} required")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1}")]
         public string Name { get; set; }
+
+        [EmailAddress (ErrorMessage = "Enter a valid email")]
+        [Required(ErrorMessage = "{0} required")]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
+        [Range (300.0, 50000.0, ErrorMessage ="{0} must be from {1} to {2}")]
+        [Required(ErrorMessage = "{0} required")]
+        [Display(Name = "Base Salary")]
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
+        [Display(Name = "Birth Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
         public Department Department { get; set; }
+
+        [Display(Name = "Department")]
         public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
         
