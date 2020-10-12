44f5c1ae57252bf141ed15803f5d418b55f1b0c4
Lucas655 <desouza655@gmail.com>
2020-10-12 14:32:50 -0300

    Implementando busca simples

---
+++ VendasWebMvc/Models/Services/SalesRecordService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VendasWebMvc.Data;
+
+namespace VendasWebMvc.Models.Services
+{
+    public class SalesRecordService
+    {
+
+        private readonly VendasWebMvcContext _context;
+
+
+        public SalesRecordService(VendasWebMvcContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .ToListAsync();
+
+        }
+
+    }
+}
